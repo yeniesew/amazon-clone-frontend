9094b8f0f56f0304db0eae97f31321a1ac91319b
yeniesew <yebrielij@gmail.com>
2025-05-14 21:18:57 -0700

    Amazon clone deployment

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,28 +1,38 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Productcard from './ProductCard';
-import classes from "./
+import Productcard from './ProductCard'
+import classes from "./product.module.css";
+import Loader from "../loader/Loader";
 
 function Product() {
-  const [product, setProduct] = useState([]); // Initialize with empty array
-
+  const [product, setProduct] = useState();
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     axios
       .get("https://fakestoreapi.com/products")
       .then((res) => {
+        //console.log(res);
         setProduct(res.data);
+        setIsLoading(false);
       })
       .catch((err) => {
         console.log(err);
+        setIsLoading(false);
       });
   }, []);
-
   return (
-    <div className={classes.product_container}>
-      {product.map((singleProduct) => (
-        <Productcard product={singleProduct} key={singleProduct.id} />
-      ))}
-    </div>
+    <>
+     {
+     isLoading? (<Loader />) : (
+      <div className={classes.product_container}>
+          {product?.map((singleProduct) => (
+            <Productcard product={singleProduct} key={singleProduct.id} renderadd={true} />
+          ))}
+        </div>
+     )
+     }
+        
+    </>
   );
 }
 
