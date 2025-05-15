3903045bd3c0a9c36821a70e2b66cb4e2978348d
yeniesew <yebrielij@gmail.com>
2025-05-14 21:05:29 -0700

    Amazone clone deployment

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Productcard from './ProductCard';
-import classes from "./Product.module.css";
+import classes from "./
 
 function Product() {
   const [product, setProduct] = useState([]); // Initialize with empty array
